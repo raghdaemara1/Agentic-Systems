e13cfe864c04ab565f12aa5bd886a1b931ca52ab
Raghda Emara <47552563+raghdaemara1@users.noreply.github.com>
2026-02-18 11:34:31 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AgenticDemo from './agentic_demo.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AgenticDemo />
+  </React.StrictMode>,
+)
